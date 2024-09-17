8fb8646b374dba77a692036cfd57e9980d6e7f10
AlessioConsolo <alessioconsolo3@gmail.com>
2024-09-17 18:47:31 +0200

    [UPD] Added construct

--- index.php
+++ index.php
@@ -5,5 +5,12 @@
         public $director;
         public $year;
         public $genre;
+
+        public function __construct($title, $director, $year, $genre) {
+            $this->title = $title;
+            $this->director = $director;
+            $this->year = $year;
+            $this->genre = $genre;
+        }
     }
 ?>
