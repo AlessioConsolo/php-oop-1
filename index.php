d93a23360f0d7f35596543d3c5f8d5ff416dbdc0
AlessioConsolo <alessioconsolo3@gmail.com>
2024-09-17 18:49:37 +0200

    [UPD] Added 2 objects called movie1/movie2

--- index.php
+++ index.php
@@ -1,23 +1,31 @@
 <?php
-    class Movie {
-        
-        public $title;
-        public $director;
-        public $year;
-        public $genre;
 
-        public function __construct($title, $director, $year, $genre) {
-            $this->title = $title;
-            $this->director = $director;
-            $this->year = $year;
-            $this->genre = $genre;
-        }
+class Movie {
+    
+    public $title;
+    public $director;
+    public $year;
+    public $genre;
 
-        public function getMovieInfo() {
-            return "Title: " . $this->title . "<br>" .
-                "Director: " . $this->director . "<br>" .
-                "Year: " . $this->year . "<br>" .
-                "Genre: " . $this->genre . "<br><br>";
-        }
+    public function __construct($title, $director, $year, $genre) {
+        $this->title = $title;
+        $this->director = $director;
+        $this->year = $year;
+        $this->genre = $genre;
     }
+
+    public function getMovieInfo() {
+        return "Title: " . $this->title . "<br>" .
+               "Director: " . $this->director . "<br>" .
+               "Year: " . $this->year . "<br>" .
+               "Genre: " . $this->genre . "<br><br>";
+    }
+}
+
+$movie1 = new Movie("Us", "Jordan Peele", 2019, "Horror/Mystery");
+$movie2 = new Movie("The Irishman", "Martin Scorsese", 2019, "Crime/Mystery");
+
+echo $movie1->getMovieInfo();
+echo $movie2->getMovieInfo();
+
 ?>
